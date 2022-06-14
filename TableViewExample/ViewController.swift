45d2b44bbf4fa57b074e3f8b4ed7bee7cd1819dd
Mshaharyar_khan <shaharyar@neeopalmmessenger.com>
2022-06-14 12:09:33 +0500

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Dawood on 14/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
